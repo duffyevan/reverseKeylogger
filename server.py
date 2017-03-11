0365a279c6505456cb84fd991868955e5e0df7be
duffyevan <eduffy2015@spyponders.com>
2017-03-10 23:39:22 -0500

    Wrote initial program

---
+++ server.py
@@ -0,0 +1,16 @@
+#import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 112233
+s.bind((host,port))
+
+s.listen(5)
+while True:
+	c,addr = s.accept()
+	print "Got Connection From Client at ", addr
+	while True:
+		rchar = s.recv(1);
+		if rchar:
+			print rchar
+
