bcf58ed7127964d19d57005b543cc723fff787c0
duffyevan <eduffy2015@spyponders.com>
2017-03-11 00:42:37 -0500

    tweaks

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 import socket
+import sys
 
-s = socket.socket()
-host = ''
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "localhost"
 port = 11223
+print "Binding Server to host:", host, ":: port:", port
 s.bind((host,port))
-
+print "Listening"
 s.listen(5)
 while True:
 	c,addr = s.accept()
 	print "Got Connection From Client at", addr
 	while True:
-		rchar = s.recv(1);
+		rchar = s.recv(100).decode("ascii")
 		if rchar:
 			print rchar
 
